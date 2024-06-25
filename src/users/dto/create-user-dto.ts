9a86e75ff31bf0e114fa401f4f588d0e0485450a
delapaska <delapaska12@yandex.ru>
2024-06-25 16:18:48 +0300

    Add pipes

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
+import {IsEmail, IsString, Length} from "class-validator";
+ 
 
 export class CreateUserDto{
     @ApiProperty({example:'qwerty123@mail.ru', description: 'User email'})
+    @IsString({message: "must be a string"})
+    @IsEmail({},{message: "Uncorrect email"})
     readonly email: string; 
 
+  
     @ApiProperty({example:'102031501112', description: 'User password'})
+    @IsString({message: "must be a string"})
+    @Length(4,16,{message:'The password must contain at least 4x and no more than 16 characters'})
     readonly password: string;
 }
